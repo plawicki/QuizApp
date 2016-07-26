f18b95ed94905d175085d655c57f5cf9690dd5e8
ITK developer User <itkdevuser@its004.local>
2016-07-26 12:52:27 +0200

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by ITK developer User on 26.07.2016.
+//  Copyright © 2016 ITK developer User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
