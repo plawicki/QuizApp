b829d5e59453d577b50139adca8f9c2cf523f43f
ITK developer User <itkdevuser@ITS004.local>
2016-07-26 13:38:32 +0200

    Inited project, added Question and Quiz entities to coredata, added QuizesTableVC and QuizTableViewCell

---
+++ QuizApp/ViewControllers/QuizTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuizTableViewCell.swift
+//  QuizApp
+//
+//  Created by ITK developer User on 26.07.2016.
+//  Copyright © 2016 ITK developer User. All rights reserved.
+//
+
+import UIKit
+
+class QuizTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
