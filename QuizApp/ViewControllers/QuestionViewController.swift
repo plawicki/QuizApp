a350edeaf6d0fdbfcb8d6df2b1a64541bfae8919
ITK developer User <itkdevuser@ITS004.local>
2016-07-27 18:55:21 +0200

    added buttons, actions, label, progressbar to Question View Controller

--- QuizApp/ViewControllers/QuestionViewController.swift
+++ QuizApp/ViewControllers/QuestionViewController.swift
@@ -6,8 +6,13 @@
 //  Copyright © 2016 ITK developer User. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class QuestionViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
 
+    @IBAction func clickAnswer(sender: UIButton) {
+        
+    }
 }
